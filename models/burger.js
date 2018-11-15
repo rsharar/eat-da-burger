68867be9bb4629d78b97dd06d52dd331f5f28173
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-14 20:45:44 -0800

    added orm.selectAll method

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,16 @@
 // Import and require ORM object
-var orm = require('../config/orm');
+var orm = require('../config/orm.js');
 
 
 // code to call the ORM functions using the burger specific input for the ORM
-
-
+var burger = {
+    all: function(callback2){
+    // "burgers" will be table name used in SQL queries
+        orm.selectAll("burgers", function(res){
+            callback2(res);
+        })
+}
+}
 
 
 // export 
