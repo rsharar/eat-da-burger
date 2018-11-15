caaf9dd063bfd049c196ab46109e52a729fd23d4
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-14 20:49:33 -0800

    added obj to store burgers data key value pair which loads index file successfully

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,18 @@ var burger = require("../models/burger");
 // Require express package
 var express = require('express');
 
-// erxpress 4.0 documentation
+// express 4.0 documentation
 var router = express.Router();
 
+// create route for index.handlebars
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var obj = {
+            burgers: data
+        }
+        // render index.handlebars with data object
+        res.render("index", obj);
+    })
+})
 
 module.exports = router;
