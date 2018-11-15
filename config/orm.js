d36ad98c33cda5c5c3d6d1d7817155e54692a1a0
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-14 20:44:49 -0800

    added select, update and insert methods

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,86 @@
 // Import MySQL connection.
 var connection = require("./connection.js");
 
+// generate questionmarks to use in SQL queries
+function generateQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
 
 
-// create 3 methods
-var orm;
-// selectAll()
-// insertOne()
-// updateOne()
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
 
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
 
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+// ORM with 3 methods
+var orm = {
+    // select all burgers in the burgers table
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            else {
+                console.log(result);
+                cb(result);
+            }
+        })
+    },
+    // add a new burger to the burgers table using user input
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + tableInput;
+        queryString += "(" + cols.toString();
+        queryString += ")";
+        queryString += " VALUES (" + generateQuestionMarks(vals.length);
+        queryString += ") ";
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            else {
+                console.log(result);
+                cb(result);
+            }
+        })
+    },
+    // change the state of a burger to devoured when 'devoured' btn is clicked
+    updateOne: function (table, obColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET"
+        queryString += objToSql(obColVals);
+        queryString += condition;
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            else {
+                console.log(result);
+                cb(result);
+            }
+        })
+    }
+}
 // export orm object
 module.exports = orm;
