14c67063ff0d61c666476d67006a9cba9ac9d64f
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-14 21:01:49 -0800

    added corresponding all, create and update methods to burger object

--- models/burger.js
+++ models/burger.js
@@ -4,13 +4,27 @@ var orm = require('../config/orm.js');
 
 // code to call the ORM functions using the burger specific input for the ORM
 var burger = {
-    all: function(callback2){
+    all: function(cb){
     // "burgers" will be table name used in SQL queries
         orm.selectAll("burgers", function(res){
-            callback2(res);
+            console.log(res);
+            cb(res);
         })
+    },
+    create: function(cols, vals, cb){
+        orm.insertOne("burgers", cols, vals, function(res){
+            console.log(res);
+            cb(res);
+        })
+    },
+    update: function(obColVals, condition, cb){
+        orm.updateOne("burgers", obColVals, condition, function(res){
+            console.log(res);
+            cb(res);
+        })
+    }
 }
-}
+
 
 
 // export 
