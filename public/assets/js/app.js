0c5ded93bde8719a02d54ed626231c2950077d67
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-17 07:53:09 -0800

    added on click of devoured btn with ajax PUT and DELETE routes using burgerId

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $(".btn-danger").on('click', function(e){
+$(document).ready(function () {
+    $(".btn-danger").on('click', function (e) {
         e.preventDefault();
         var newBurger = {
             burger_name: $("#burger-input").val().trim()
@@ -9,9 +9,35 @@ $(document).ready(function(){
             type: "POST",
             data: newBurger
         }).then(
-            function(){
-            console.log("Added burger");
-            location.reload();
+            function () {
+                console.log("Added burger");
+                location.reload();
+            })
+    })
+    $(".devour-btn").on('click', function (e) {
+        e.preventDefault();
+        var burgerId = $(this).data("id");
+        var devour = $(this).data("devour");
+        if (devour) {
+            var newDevouredState = {
+                devoured: devour
+            };
+            $.ajax("/api/burgers" + burgerId, {
+                type: "PUT",
+                data: newDevouredState
+            }).then(function () {
+                console.log("devoured state changed");
+                location.reload();
+            })
+        }
+        else {
+            $.ajax("/api/burgers" + burgerId, {
+                type: "DELETE"
+            }).then(function () {
+                console.log("Burger gone")
+                location.reload();
+            })
+        }
     })
 })
-})
+
